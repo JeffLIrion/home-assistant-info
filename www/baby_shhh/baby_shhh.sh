dfefee8987d28a6fa41f4b6c16134c292245d1e0
Jeff Irion <jefflirion@users.noreply.github.com>
2020-04-04 11:43:29 -0700

    Make the volume louder

--- www/baby_shhh/baby_shhh.sh
+++ www/baby_shhh/baby_shhh.sh
@@ -11,3 +11,6 @@ for i in {1..60}; do echo "file 'baby_shhh1.mp3'" >> baby_shhh.txt; done
 
 # 3. Loop the audio clip 60 times
 ffmpeg -t 3600.0 -f concat -i baby_shhh.txt -c copy -t 3600.0 baby_shhh.mp3
+
+# 4. Make it louder
+ffmpeg -i baby_shhh.mp3 -filter:a "volume=2.0" baby_shhh2.mp3
