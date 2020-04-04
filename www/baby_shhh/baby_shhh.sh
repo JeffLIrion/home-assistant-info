296beb8f49b71aad3b76bafa4f0cc829d77013b6
Jeff Irion <jefflirion@users.noreply.github.com>
2020-04-04 11:51:54 -0700

    Make the volume 3x the original

--- www/baby_shhh/baby_shhh.sh
+++ www/baby_shhh/baby_shhh.sh
@@ -10,7 +10,7 @@ ffmpeg -i baby_shhh.MOV -f mp3 -ab 192000 -vn baby_shhh1.mp3
 for i in {1..60}; do echo "file 'baby_shhh1.mp3'" >> baby_shhh.txt; done
 
 # 3. Loop the audio clip 60 times
-ffmpeg -t 3600.0 -f concat -i baby_shhh.txt -c copy -t 3600.0 baby_shhh.mp3
+ffmpeg -t 3600.0 -f concat -i baby_shhh.txt -c copy -t 3600.0 baby_shhh2.mp3
 
 # 4. Make it louder
-ffmpeg -i baby_shhh.mp3 -filter:a "volume=2.0" baby_shhh2.mp3
+ffmpeg -i baby_shhh2.mp3 -filter:a "volume=3.0" baby_shhh.mp3
